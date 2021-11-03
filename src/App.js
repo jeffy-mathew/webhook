e7909cb9b7d9c5af270ca975892faf732172a3e0
Jeff <jeffy.mathew100@gmail.com>
2021-11-03 20:17:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+const App = () => {
+  const [requests, setRequests] = useState([]);
+  useEffect(() => {
+    axios.get("https://c1a1aa44-4766-472c-9557-58c282598e43.mock.pstmn.io/requests").then((response) => {
+      console.log("request response", response);
+      setRequests(response.data);
+    });
+  }, []);
+return (
+  
+  <Layout  style={{height:"100vh"}}>
+      <Sider width={200} className="site-layout-background" >
+      
+        <Menu theme="dark" mode="vertical" defaultSelectedKeys={['2']}>
+        {
+        requests.map(request => (
+          <Menu.Item key={request.id} onClick={()=> {console.log("clicked", request.id, request.method)} }>{request.id}</Menu.Item>
+        ))
+        }
+        </Menu>
+      
+      </Sider>
+  </Layout>
+);
 }
 
-export default App;
+export default App
