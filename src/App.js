6fa05e82b106ef96f5628aa32f8a6998f2bcb099
jeff <jeffy.mathew@getsimpl.com>
2021-11-04 10:25:23 +0530

    improve ui

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Layout, Menu, Card, Badge } from "antd";
+const { Sider } = Layout;
 
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const getBadgeColor = (method) => {
+  switch (method) {
+    case "POST":
+      return "red";
+    case "GET":
+      return "green";
+    case "PUT":
+      return "pink";
+    default:
+      return "green";
+  }
+};
 const App = () => {
   const [requests, setRequests] = useState([]);
   useEffect(() => {
-    axios.get("https://c1a1aa44-4766-472c-9557-58c282598e43.mock.pstmn.io/requests").then((response) => {
-      console.log("request response", response);
-      setRequests(response.data);
-    });
+    axios
+      .get(
+        "https://178fb235-c752-494b-8bd5-97f48179e8fa.mock.pstmn.io/requests"
+      )
+      .then((response) => {
+        console.log("request response", response);
+        setRequests(response.data);
+      });
   }, []);
-return (
-  
-  <Layout  style={{height:"100vh"}}>
-      <Sider width={200} className="site-layout-background" >
-      
-        <Menu theme="dark" mode="vertical" defaultSelectedKeys={['2']}>
-        {
-        requests.map(request => (
-          <Menu.Item key={request.id} onClick={()=> {console.log("clicked", request.id, request.method)} }>{request.id}</Menu.Item>
-        ))
-        }
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider width={400} className="site-layout-background">
+        <Menu theme="dark" mode="vertical">
+          {requests.map((request) => (
+            <Menu.Item
+              style={{ height: "20vh" }}
+              key={request.id}
+              onClick={() => {
+                console.log("clicked", request.id, request.method);
+              }}
+            >
+              <Badge.Ribbon
+                text={request.method}
+                color={getBadgeColor(request.method)}
+              >
+                <Card title={request.id} size="small">
+                  {request.epoch_timestamp}
+                </Card>
+              </Badge.Ribbon>
+            </Menu.Item>
+          ))}
         </Menu>
-      
       </Sider>
-  </Layout>
-);
-}
+    </Layout>
+  );
+};
 
-export default App
+export default App;
